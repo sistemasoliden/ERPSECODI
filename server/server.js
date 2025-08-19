ce9753b4c3bdecdae661efd32ae3efea19ef49fd
sistemasoliden <sistemas.ti1102@gmail.com>
2025-08-19 10:33:38 -0500

    Avance 19/08/2025

--- server/server.js
+++ server/server.js
@@ -2,30 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import { fileURLToPath } from "url";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import ventasRoutes from "./routes/ventasRoutes.js";
 import ejecutivesRoutes from "./routes/ejecutivesRoutes.js";
 
-import User from "./models/User.js";
-import bcrypt from "bcryptjs";
-
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-if (process.env.NODE_ENV === "production") {
-    const clientPath = path.join(__dirname, "../frontend/dist");
-  app.use(express.static(clientPath));
-
-  // Cualquier ruta que no sea API debe devolver index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(clientPath, "index.html"));
-  });
-}
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -33,35 +18,42 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors({
   origin: [
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "https://erpsecodi.onrender.com"
   ],
   credentials: true
 }));
-
 app.use(express.json());
 
-// Rutas
+// Rutas API
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/ventas", ventasRoutes);
 app.use("/api/ejecutivos", ejecutivesRoutes);
 
-
-app.get("/", (req, res) => {
+// Ruta básica backend
+app.get("/api", (req, res) => {
   res.send("ERP Backend conectado a MongoDB Atlas");
 });
 
-// Conexión a MongoDB
+// 👉 Servir frontend (solo en producción)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  const clientPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(clientPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
+
+// Conexión MongoDB y levantar servidor
 mongoose.connect(process.env.MONGO_URI)
-.then(async () => {
-  console.log("✅ Conectado a MongoDB Atlas");
-
-  
-
-  // Inicia el servidor
-  app.listen(PORT, () =>
-    console.log(`🚀 Servidor backend escuchando en http://localhost:${PORT}`)
-  );
-})
-.catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
+  .then(() => {
+    console.log("✅ Conectado a MongoDB Atlas");
+    app.listen(PORT, () =>
+      console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
