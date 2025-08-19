28c57748a6f35cf556f204a0aefeccf82d8cbe8a
sistemasoliden <sistemas.ti1102@gmail.com>
2025-08-19 10:11:52 -0500

    Avance 19/08/2025

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://erp-frontend.onrender.com"
+    "https://erpsecodi.onrender.com"
   ],
   credentials: true
 }));
